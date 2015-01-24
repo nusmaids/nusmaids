748cf19fc0fdc50b0ca6d0a63fb652f39ba23356
Andrew Low <andrewjrlow@gmail.com>
2015-01-25 01:05:48 +0800

    a lot of stuff

--- create-job.php
+++ create-job.php
@@ -1,46 +1,64 @@
 <!DOCTYPE html>
 <html>
 <head lang="en">
-    <link type="text/css" rel="stylesheet" href="nusstyles.css" />
+    <link type="text/css" rel="stylesheet" href="css/nusstyles.css" />
+    <link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
     <meta charset="UTF-8">
     <title></title>
 </head>
 <body>
-    <div class="container" id="create-form-container">
+    <div class="container form-create-container" id="create-form-container">
         <form class="form-horizontal" id="create-form-horizontal">
-            <div class="radio-type" id="create-radio-type">
-                <label class="radio-inline">
-                    <input type="radio" id="radioCleaning" value="option1">Cleaning
-                </label>
-                <label class="radio-inline">
-                    <input type="radio" id="radioRepairs" value="option2">Repairs
-                </label>
-                <label class="radio-inline">
-                    <input type="radio" id="radioLabour" value="option3">Labour
-                </label>
-                <label class="radio-inline">
-                    <input type="radio" id="radioHandicap" value="option4">Handicap Assistance
-                </label>
+
+            <div class="form-create-header" id="create-job-type">
+                <label class="create-form-group">Job type</label>
             </div>
-            <div class="form-create">
-                <label class="form-group">Location</label>
+
+            <div class="create-dropdown" id="create-dropdown">
+                <select class="form-control create-select" id="create-select-type">
+                    <option>Cleaning</option>
+                    <option>Repairs</option>
+                    <option>Labour</option>
+                    <option>Handicap Assistance</option>
+                </select>
+            </div>
+
+            <div class="form-create-header">
+                <label class="create-form-group">Remuneration (in SGD)</label>
+            </div>
+            <div class="form-create-input">
+                <input type="text" class="form-control" id="inputRemuneration" placeholder="Expected pay">
+            </div>
+
+            <div class="form-create-header">
+                <label class="create-form-group">Location</label>
+            </div>
+            <div class="form-create-input">
                 <input type="text" class="form-control" id="inputLocation" placeholder="Your job location">
             </div>
-            <div class="form-create">
-                <label class="form-group">Starting from</label>
-                <div></div>
-                <input class="datePicker" date-date-format="dd/mm/yyyy"/>
+
+            <div class="form-create-header">
+                <label class="create-form-group">Starting from</label>
             </div>
-            <div class="form-create">
-                <label class="form-group">Ending at</label>
-                <div></div>
-                <input class="datePicker" date-date-format="dd/mm/yyyy"/>
+            <div class="form-create-input">
+                <input type="text" class="form-control" id="inputStartDate" placeholder="DD/MM/YYYY">
             </div>
-            <div class="form-create">
-                <label class="form-group">Description</label>
+
+            <div class="form-create-header">
+                <label class="create-form-group">Ending at</label>
+            </div>
+            <div class="form-create-input">
+                <input type="text" class="form-control" id="inputEndDate" placeholder="DD/MM/YYYY">
+            </div>
+
+            <div class="form-create-header">
+                <label class="create-form-group">Description</label>
+            </div>
+            <div class="form-create-input">
                 <input type="text" class="form-control" id="inputDescription" placeholder="Your job description">
             </div>
-            <button type="submit" class="btn btn-submit">Submit Form</button>
+
+            <button type="submit" class="btn btn-submit" id="createSubmitButton">Submit Form</button>
         </form>
     </div>
 </body>
