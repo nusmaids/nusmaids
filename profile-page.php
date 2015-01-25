bf7528e90402e39b53d12706ff4271bcc3e18816
Andrew Low <andrewjrlow@gmail.com>
2015-01-25 09:39:05 +0800

    commit

---
+++ profile-page.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <?php
+    include("layout/login-redirect.php");
+    include("layout/head-template.php");
+    ?>
+
+</head>
+
+<body>
+    <?php
+    include("layout/navbar.php");
+    ?>
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1>NUSMaids</h1>
+            <h3>Your NUSMaids profile page</h3>
+        </div>
+        <hr>
+        <div class="container">
+
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
