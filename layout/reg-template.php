0bb6e3323732d3629bde3c1c3d811c4a5b15c128
Nicholas Lum <nicholaslum444@hotmail.com>
2015-01-25 11:18:54 +0800

    update names for login and reg

--- layout/reg-template.php
+++ layout/reg-template.php
@@ -14,7 +14,7 @@
                 <label class="create-form-group">NUS Email</label>
             </div>
             <div class="form-create-input">
-                <input type="email" class="form-control" id="input-nus-email" placeholder="a1234567@nus.edu.sg">
+                <input type="email" name="registerEmail" class="form-control" id="input-nus-email" placeholder="a1234567@nus.edu.sg">
             </div>
         </div>
 
@@ -32,7 +32,7 @@
                 <label class="create-form-group">Confirm Password</label>
             </div>
             <div class="form-create-input">
-                <input type="password" class="form-control" id="input-password-confirm" onChange="checkPasswordMatch();">
+                <input type="password" name="registerPassword" class="form-control" id="input-password-confirm" onChange="checkPasswordMatch();">
             </div>
         </div>
 
@@ -45,7 +45,7 @@
                 <label class="create-form-group">Contact Number</label>
             </div>
             <div class="form-create-input">
-                <input type="tel" class="form-control" id="input-contact-number" placeholder="90785634">
+                <input type="tel" name="registerContactNumber" class="form-control" id="input-contact-number" placeholder="90785634">
             </div>
         </div>
 
@@ -54,7 +54,7 @@
                 <label class="create-form-group">First Name</label>
             </div>
             <div class="form-create-input">
-                <input type="text" class="form-control" id="input-first-name" placeholder="John">
+                <input type="text" name="registerFirstName" class="form-control" id="input-first-name" placeholder="John">
             </div>
         </div>
 
@@ -63,11 +63,11 @@
                 <label class="create-form-group">Last Name</label>
             </div>
             <div class="form-create-input">
-                <input type="text" class="form-control" id="input-last-name" placeholder="Smith">
+                <input type="text" name="registerLastName" class="form-control" id="input-last-name" placeholder="Smith">
             </div>
         </div>
 
-        <button type="submit" class="btn btn-primary btn-lg btn-success nusmaids-form-button" id="create-account-button">Create Account</button>
+        <button type="submit" name="registerSubmit" class="btn btn-primary btn-lg btn-success nusmaids-form-button" id="create-account-button">Create Account</button>
 
     </form>
 </div>
