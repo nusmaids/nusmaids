c3494ece6ee5dd23e3ab20f328b5c00d15e3d981
Nicholas Lum <nicholaslum444@hotmail.com>
2015-01-25 09:40:11 +0800

    fixed index

--- index.php
+++ index.php
@@ -20,29 +20,32 @@
             <h3>The platform that solves your odd-job problems</h3>
         </div>
         <hr>
-        <div class="container" style="border: 1px solid black">
+        <div class="container" id="selection-container">
+            <div class="row">
 
-            <div class="container-fluid nusmaids-selection-box" id="find-jobs-box">
+                <div class="col-md-6 nusmaids-selection-box" id="find-jobs-box">
 
-                <h3>Be A Maid</h3>
-                <a href="find-job.php?j=1"><div class="nusmaids-selection-link">Cleaning</div></a>
-                <a href="find-job.php?j=2"><div class="nusmaids-selection-link">Repairs</div></a>
-                <a href="find-job.php?j=3"><div class="nusmaids-selection-link">Labour</div></a>
-                <a href="find-job.php?j=4"><div class="nusmaids-selection-link">Handicap Assistance</div></a>
+                    <h3>Be A Maid</h3>
+                    <a href="find-job.php?j=1"><div class="btn btn-default nusmaids-selection-link">Cleaning</div></a><br>
+                    <a href="find-job.php?j=2"><div class="btn btn-default nusmaids-selection-link">Repairs</div></a><br>
+                    <a href="find-job.php?j=3"><div class="btn btn-default nusmaids-selection-link">Labour</div></a><br>
+                    <a href="find-job.php?j=4"><div class="btn btn-default nusmaids-selection-link">Handicap Assistance</div></a>
+
+                </div>
+
+                <div class="col-md-6 nusmaids-selection-box" id="create-jobs-box">
+
+                    <h3>Create A Job</h3>
+                    <a href="create-job.php?j=1"><div class="btn btn-default nusmaids-selection-link">Cleaning</div></a>
+                    <a href="create-job.php?j=2"><div class="btn btn-default nusmaids-selection-link">Repairs</div></a>
+                    <a href="create-job.php?j=3"><div class="btn btn-default nusmaids-selection-link">Labour</div></a>
+                    <a href="create-job.php?j=4"><div class="btn btn-default nusmaids-selection-link">Handicap Assistance</div></a>
+
+                </div>
 
             </div>
-
-            <div class="container-fluid nusmaids-selection-box" id="create-jobs-box">
-
-                <h3>Create A Job</h3>
-                <a href="create-job.php?j=1"><div class="nusmaids-selection-link">Cleaning</div></a>
-                <a href="create-job.php?j=2"><div class="nusmaids-selection-link">Repairs</div></a>
-                <a href="create-job.php?j=3"><div class="nusmaids-selection-link">Labour</div></a>
-                <a href="create-job.php?j=4"><div class="nusmaids-selection-link">Handicap Assistance</div></a>
-
-            </div>
-
         </div>
+
     </div>
 
 </body>
